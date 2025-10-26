41e529448fb09da93305cd976eaa5dcf881c7fb6
Matheus <mps.web.tech@gmail.com>
2025-10-26 10:55:13 -0300

    feat: configure mantine

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { MantineProvider } from "@mantine/core";
+import '@mantine/core/styles.layer.css';
+
 import { Router } from "./Routes/Router";
 
 function App() {
   return (
-    <>
+    <MantineProvider>
       <Router />
-    </>
+    </MantineProvider>
   );
 }
 
